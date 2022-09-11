4fba64546ab2f457895824cf2b38493e91c1239a
googlekeep <info.workingforyou@gmail.com>
2022-09-11 23:41:08 +0530

    array

---
+++ array.js
@@ -0,0 +1,5 @@
+var friendsAge = [11, 15, 30, 85, 74, 7];
+var picnicFee = [5000, 2000, 6000];
+var nayikas = [ 'mahi', 'apu', 'poop'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowels);
