53b9d095dd31d6c4ffc87a38ceb77858681fbc39
googlekeep <info.workingforyou@gmail.com>
2022-09-12 20:52:39 +0530

    index setup

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ var numbers = [45, 68, 78, 56, 98];
 
 
 // get element by index 
+var element = number [1];
 
-console.log(numbers [0])
+console.log(element);
+ 
+// 2. set element value by index
+numbers[1] = 77;
+console.log(numbers);
+
+// 3 find index of an elementlement
+
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
 
 
